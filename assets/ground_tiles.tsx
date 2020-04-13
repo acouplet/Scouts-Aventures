84b047087700382b80e51c09b7edbc484077aaf9
madame-lugovine <lugovine.louchez@gmail.com>
2020-04-13 20:03:22 +0200

    Add assets

---
+++ assets/ground_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="ground_tiles" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="ground_tiles.png" width="592" height="592"/>
+</tileset>
